d316df65cf155276b268b49379abf07ac134a960
Alan Mesquita <alan.mesquita@dafiti.com.br>
2013-11-11 16:14:49 -0200

    Tests refatorados

--- numbers/lib/Number.rb
+++ numbers/lib/Number.rb
@@ -1,14 +1,16 @@
 class Number
 
-    @@unit = ['zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove']
-    @@dozen = ['dez', 'vinte', 'trinta', 'quarenta', 'cinquenta', 'sessenta', 'setenta', 'oitenta', 'noventa']
-    @@exceptions = ['onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezeseis', 'dezesete', 'dezoito', 'dezenove']
+    def initialize ()
+        @@unit = [ 'zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove' ]
+        @@dozen = [ 'dez', 'vinte', 'trinta', 'quarenta', 'cinquenta', 'sessenta', 'setenta', 'oitenta', 'noventa' ]
+        @@exceptions = [ 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezeseis', 'dezesete', 'dezoito', 'dezenove' ]
+    end
 
     def run (number)
         number = number.to_s
         if(number.size == 2)
             if(positionNumber(number) == 1 and positionNumber(number, 1) > 0)
-                return @@exceptions[number.to_i-11]
+                return @@exceptions[number.to_i - 11]
             end
 
             if (positionNumber(number, 1) > 0)
--- numbers/spec/Author_spec.rb
+++ numbers/spec/Author_spec.rb
@@ -2,49 +2,44 @@ require "Number"
 
 describe Number do
 
-    it 'deveria rtornar Um' do
-        author = Number.new()
-        author.run(1).should eql('um')
+    before :all do
+        @number = Number.new();
     end
 
-    it 'deveria retornar dois' do
-        author = Number.new()
-        author.run(2).should eql('dois')
+    it 'retorna Um' do
+        @number.run(1).should eql('um')
     end
 
-    it 'deveria retornar tres' do
-        author = Number.new()
-        author.run(3).should eql('tres')
+    it 'retorna dois' do
+        @number.run(2).should eql('dois')
     end
 
-    it 'deveria retornar 20' do
-        author = Number.new()
-        author.run(20).should eql('vinte')
+    it 'retorna tres' do
+        @number.run(3).should eql('tres')
     end
 
-    it 'deveria retornar 30' do
-        author = Number.new()
-        author.run(30).should eql('trinta')
+    it 'retorna vinte' do
+        @number.run(20).should eql('vinte')
     end
 
-    it 'deveria retornar 33' do
-        author = Number.new()
-        author.run(33).should eql('trinta e tres')
+    it 'retorna trinta' do
+        @number.run(30).should eql('trinta')
     end
 
-    it 'deveria retornar 44' do
-        author = Number.new()
-        author.run(44).should eql('quarenta e quatro')
+    it 'retornar trinta e tres' do
+        @number.run(33).should eql('trinta e tres')
     end
 
-    it 'deveria retornar 44' do
-        author = Number.new()
-        author.run(11).should eql('onze')
+    it 'retorna quarenta e quatro' do
+        @number.run(44).should eql('quarenta e quatro')
     end
 
-    it 'deveria retornar 44' do
-        author = Number.new()
-        author.run(10).should eql('dez')
+    it 'retorna onze' do
+        @number.run(11).should eql('onze')
+    end
+
+    it 'retorna dez' do
+        @number.run(10).should eql('dez')
     end
 
 end
